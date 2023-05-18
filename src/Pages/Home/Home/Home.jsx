fb1637ba7890ce43cf3f3d6602200f7e253f485e
Arib Mahmud <aribmahmud27@gmail.com>
2023-05-18 17:31:49 +0600

    first commit

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+import Trending from '../Trending/Trending';
+
+const Home = () => {
+    return (
+        // <div>
+        //     <Banner></Banner>
+        //     <Trending></Trending>
+        // </div>
+    );
+};
+
+export default Home;
