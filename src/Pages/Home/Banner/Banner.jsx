58239b50daeff08b75576eb47ce4de333df6c9b1
Arib Mahmud <aribmahmud27@gmail.com>
2023-05-18 18:01:13 +0600

    Banner section added

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -3,23 +3,23 @@ import bannerImg from "../../../assets/2.webp";
 
 const Banner = () => {
   return (
-    // <div className=" bg-[#C2E0EB] w-full">
-    //   <div className="hero min-h-screen container">
-    //     <div className="hero-content flex-col lg:flex-row-reverse">
-    //       <img
-    //         src={bannerImg}
-    //         className="md:max-w-2xl max-w-xs"
-    //       />
-    //       <div>
-    //         <h1 className="text-5xl md:text-7xl font-bold">Best Kid Store & Online Shop</h1>
-    //         <p className="py-6 text-lg md:text-xl font-bold">
-    //           Give The Gift Of Your Children Everyday
-    //         </p>
-    //         <button className="btn btn-primary">Shop Now</button>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
+    <div className=" bg-[#C2E0EB] w-full">
+      <div className="hero min-h-screen container">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src={bannerImg}
+            className="md:max-w-2xl max-w-xs"
+          />
+          <div>
+            <h1 className="text-5xl md:text-7xl font-bold">Best Kid Store & Online Shop</h1>
+            <p className="py-6 text-lg md:text-xl font-bold">
+              Give The Gift Of Your Children Everyday
+            </p>
+            <button className="btn btn-primary">Shop Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
