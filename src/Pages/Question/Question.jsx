741bc1d9b38fecde06e2b3468cd754a9493b12e9
Arib Mahmud <aribmahmud27@gmail.com>
2023-05-19 01:59:02 +0600

    Third Question Has been Added

--- src/Pages/Question/Question.jsx
+++ src/Pages/Question/Question.jsx
@@ -22,42 +22,34 @@ const Question = () => {
         </div>
         {/* Question end here  */}
         <div className="mb-4">
-          <h4 className="text-xl font-bold">2. Compare SQL and NoSQL databases?</h4>
+          <h4 className="text-xl font-bold">
+            2. Compare SQL and NoSQL databases?
+          </h4>
           <p>
             <span className="font-bold">Ans:-</span>
             <span className="block">
-              1.SQL databases are vertically scalable, while NoSQL databases are
-              horizontally scalable.
+              1.SQL databases are vertically scalable, while NoSQL databases are horizontally scalable.
             </span>
             <span className="block">
-              2.SQL databases are table-based, while NoSQL databases are
-              document, key-value, graph, or wide-column stores.
+              2.SQL databases are table-based, while NoSQL databases are document, key-value, graph, or wide-column stores.
             </span>
             <span className="block">
-              3.SQL databases are better for multi-row transactions, while NoSQL
-              is better for unstructured data like documents or JSON.
+              3.SQL databases are better for multi-row transactions, while NoSQL is better for unstructured data like documents or JSON.
             </span>
           </p>
         </div>
         {/* Question end here  */}
-        {/* <div className="mb-4">
-          <h4 className="text-xl font-bold">2. Compare SQL and NoSQL databases?</h4>
+        <div className="mb-4">
+          <h4 className="text-xl font-bold">
+            3.What is express JS? What is Next JS?
+          </h4>
           <p>
-            <span className="font-bold">Ans:-</span>
-            <span className="block">
-              1.SQL databases are vertically scalable, while NoSQL databases are
-              horizontally scalable.
-            </span>
-            <span className="block">
-              2.SQL databases are table-based, while NoSQL databases are
-              document, key-value, graph, or wide-column stores.
-            </span>
-            <span className="block">
-              3.SQL databases are better for multi-row transactions, while NoSQL
-              is better for unstructured data like documents or JSON.
-            </span>
+            <span className="font-bold">ExpressJs</span> is a Node. js-based web application framework to
+            build APIs and server-side applications, on the other hand <span className="font-bold">NextJs</span> is
+            a react-based framework used for creating static websites and SEO optimizations. Therefore, they are not intended to replace one
+            another in any means or ways.
           </p>
-        </div> */}
+        </div>
         {/* Question end here  */}
       </div>
     </div>
